89bc2ade561952d00b2a24da954119247f764f42
Pedro Mendes <pedro.mendes@peixeurbano.com>
2018-10-12 01:08:05 -0300

    chore: remove commented code.

--- src/App.test.js
+++ src/App.test.js
@@ -3,18 +3,9 @@ import ReactDOM from 'react-dom'
 import BooksApp from './App'
 import {shallow} from './enzyme'
 
-/** 
- This course is not designed to teach Test Driven Development. 
- Feel free to use this file to test your application, but it 
- is not required.
-**/
-
 describe('App', () => {
   const app = shallow(<BooksApp />);
-
   it('renders without crashing', () => {
-    // const div = document.createElement('div')
-    // ReactDOM.render(<BooksApp />, div)
     expect(app).toMatchSnapshot();
   })
 })  
